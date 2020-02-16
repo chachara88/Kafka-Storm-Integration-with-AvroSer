c49e2b5c4aaf0e930715fbb9eef5fcf066b7527c
chachara88 <39692861+chachara88@users.noreply.github.com>
2020-02-16 11:11:16 +0200

    pom.xml change and DummyLoggerBolt creation

--- src/main/java/org/stormexample/simplestormTopology.java
+++ src/main/java/org/stormexample/simplestormTopology.java
@@ -1,19 +1,45 @@
 package org.stormexample;
 
-import org.apache.storm.kafka.SpoutConfig;
+import org.apache.storm.LocalCluster;
+//import org.apache.storm.kafka.SpoutConfig;
 import org.apache.storm.kafka.spout.KafkaSpout;
 import org.apache.storm.kafka.spout.KafkaSpoutConfig;
+import org.apache.storm.topology.TopologyBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
 
 
 public class simplestormTopology {
+    private static final Logger LOG = LoggerFactory.getLogger(simplestormTopology.class);
     public static void main(String[] args) throws Exception{
-        /*private static */ final Logger LOG = LoggerFactory.getLogger(simplestormTopology.class);
+//        if(args.length < 4){
+//            LOG.error("Incorrect number of arguments. Required arguments: <zk-hosts> <kafka-topic> <zk-path> <clientid>");
+//           System.exit(1);
+//        }
+
+        // Build topology to consume message from kafka and print them on console
+        final TopologyBuilder topologyBuilder = new TopologyBuilder();
+        // Create KafkaSpout instance using Kafka configuration and add it to topology
+        topologyBuilder.setSpout("kafka-spout", new KafkaSpout(( KafkaSpoutConfig.builder("localhost:6667", Pattern.compile("topic.*"))
+                .setOffsetCommitPeriodMs(10000) // KafkaSpoutConfig.ProcessingGuarantee is KafkaSpoutConfig.ProcessingGuarantee.AT_LEAST_ONCE.
+                .setMaxUncommittedOffsets(259) // KafkaSpoutConfig.ProcessingGuarantee is KafkaSpoutConfig.ProcessingGuarantee.AT_LEAST_ONCE.
+                /* processing guarantee mode is set to AT_LEAST_ONCE
+                 *  that means that many methods are set to default,
+                 *  eg: setEmitNullTuples(), setProcessingGuarantee(),
+                 *  setTupleTrackingEnforced()
+                 *  */
+//                .setProp()
+//                .setRecordTranslator() //TODO to be set accordingly
+                .build())), 1);
+        //Route the output of Kafka Spout to Logger bolt to log messages consumed from Kafka
+        topologyBuilder.setBolt("print-messages", new DummyLoggerBolt()).globalGrouping("kafka-spout");
+        final LocalCluster localCluster = new LocalCluster();
+//        localCluster.submitTopology("kafka-topology", new HashMap<>(), topologyBuilder.createTopology());
         //Wildcard topics
         // tp.setSpout("kafka_spout", new KafkaSpout<>(KafkaSpoutConfig.builder("127.0.0.1:" + port, Pattern.compile("topic.*")).build()), 1);
         Set<String> setOfTopics = new HashSet<String>(); //TODO: this can be a "setter" method outside main
@@ -50,10 +76,10 @@ public class simplestormTopology {
                  *  eg: setEmitNullTuples(), setProcessingGuarantee(),
                  *  setTupleTrackingEnforced()
                  *  */
-                .setProp()
+//                .setProp()
 //                .setRecordTranslator() //TODO to be set accordingly
                 .build());
     }
-    SpoutConfig spoutConfig = new SpoutConfig();
+
 }
 
