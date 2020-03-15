1f3336df8471497ab29128130175e919da1e5b81
chachara88 <39692861+chachara88@users.noreply.github.com>
2020-03-15 21:09:59 +0200

    Adding TesterBolt in Topology. Remove Parallelism and second Spout for testing purposes

--- src/main/java/org/stormexample/simplestormTopology.java
+++ src/main/java/org/stormexample/simplestormTopology.java
@@ -1,4 +1,8 @@
 package org.stormexample;
+import com.variacode.cep.storm.esper.EsperBolt;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.generated.Bolt;
 import org.apache.storm.generated.StormTopology;
 import org.apache.storm.topology.TopologyBuilder;
 import org.slf4j.Logger;
@@ -8,37 +12,42 @@ import java.util.*;
 
 public class simplestormTopology {
     private static final Logger LOG = LoggerFactory.getLogger(simplestormTopology.class);
-    public StormTopology build(){
-        TopologyBuilder topologyBuilder = new TopologyBuilder();
-        Set<String> setOfTopics = new HashSet<String>(); //TODO: this can be a "setter" method outside main
-        setOfTopics.add("aeroloop_AngularVelocity");
-        setOfTopics.add("aeroloop_CpuUsage");
-        setOfTopics.add("aeroloop_Current");
-        setOfTopics.add("aeroloop_FuelUsage");
-        setOfTopics.add("aeroloop_Location");
-        setOfTopics.add("aeroloop_SensorReadingScalar");
-        setOfTopics.add("aeroloop_StorageUsage");
-        setOfTopics.add("aeroloop_UAVState");
-        setOfTopics.add("aeroloop_UaVState");
-        setOfTopics.add("aeroloop_Voltage");
-        /*TODO Set will be removed once we conclude in topics, we will use*/
-        List<AvroKafkaSpout> avroSpoutList = null;
+//    public StormTopology build(){
+//        TODO to have all teh functionality here instead of main ! This way main to be a bit cleaner
+//    }
 
-        LOG.info("KAFKASPOUT: Configuring the KafkaSpout");
-        Iterator<String> topicsSetIterator = setOfTopics.iterator();
-        while (topicsSetIterator.hasNext()) { //Create the list of the spout(s) and then tehy are set in topology
-            String KafkaSpoutName = topicsSetIterator.next() + "Spout";
-            AvroKafkaSpout avroKafkaSpout = new AvroKafkaSpout(AvroKafkaSpoutConfig.builder("localhost:6667", topicsSetIterator.next())
-                    .setOffsetCommitPeriodMs(10000) // KafkaSpoutConfig.ProcessingGuarantee is KafkaSpoutConfig.ProcessingGuarantee.AT_LEAST_ONCE.
-                    .setMaxUncommittedOffsets(259)
-                    .build());
-            topologyBuilder.setSpout(KafkaSpoutName, avroKafkaSpout, 1);
-        }
-        return topologyBuilder.createTopology();
-    }
-
+//    public StormTopology submit2LocalCluster(){
+//  TODO to have all the functionality here instead of main ! This way main to be a bit cleaner
+//    }
     public static void main(String[] args) throws Exception {
+        Config config = new Config();
+        config.setDebug(true);
 
+        TopologyBuilder topologyBuilder = new TopologyBuilder();
+
+//        LOG.info("AVROKAFKASPOUT: Creating AvroKafkaSpout for aeroloop_SensorReadingScalar topic\n");
+//        AvroKafkaSpout SensorReadingKafkaSpout = new AvroKafkaSpout(AvroKafkaSpoutConfig.builder("eagle5.di.uoa.gr:9092", "aeroloop_SensorReadingScalar")
+//                .setOffsetCommitPeriodMs(10000) // KafkaSpoutConfig.ProcessingGuarantee is KafkaSpoutConfig.ProcessingGuarantee.AT_LEAST_ONCE.
+//                .setMaxUncommittedOffsets(259)
+//                .build());
+//        LOG.info("SET_TOPOLOGY_SPOUT: Setting SensorReadingSpout to the topology\n");
+//        topologyBuilder.setSpout("SensorReadingSpout",SensorReadingKafkaSpout/*, 2*/); //TODO increase num of parallelism?
+        LOG.info("AVROKAFKASPOUT: Creating AvroKafkaSpout for aeroloop_Voltage topic\n");
+        AvroKafkaSpout VoltageKafkaSpout = new AvroKafkaSpout(AvroKafkaSpoutConfig.builder("eagle5.di.uoa.gr:9092", "aeroloop_Voltage")
+                .setOffsetCommitPeriodMs(10000)
+                .setMaxUncommittedOffsets(259)
+                .build());
+        LOG.info("SET_TOPOLOGY_SPOUT: Setting VoltageKafkaSpout to the topology\n");
+        topologyBuilder.setSpout("VoltageSpout",VoltageKafkaSpout/*, 2*/); //TODO increase num of parallelism?
+        LOG.info("SET_TOPOLOGY_SPOUT: Setting tester Bolt to the topology\n");
+        topologyBuilder.setBolt("testerBolt", new testerEsperBolt(), 2)
+                .shuffleGrouping("VoltageSpout");
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("LogAnalyserStorm", config, topologyBuilder.createTopology()); //TODO to change topologyName
+        Thread.sleep(1000000000);
+
+        //Stop the topology
+        cluster.shutdown();
     }
 }
 
